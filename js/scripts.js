b92628fe91ad467cb183b59624f17fb5ab95e26b
Alena Golovina and Saul Artiga <saul@>
2017-05-31 14:26:21 -0700

    create skeleton project file tree

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
 $(document).ready(function() {
-  $("#formOne").submit(function() {
-    var nameInput = $("input#name").val();
-    $(".name").text(nameInput);
-    $("#story").show();
-    event.preventDefault();
-  });
+  
 });
