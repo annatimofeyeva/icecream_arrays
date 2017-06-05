1730ed6cee684685e536dfa6d484b25e6d46f071
Alena Golovina and Anna Timofeeva <anna@>
2017-06-05 14:59:19 -0700

    add js looping: list of flavors

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-  
+
+var flavors = ["chocolate", "strawberry", "vanilla", "pistachio" ];
+flavors.forEach(function(flavor) {
+  $("ul").append("<li>"+ flavor + "</li>");
+
+});
 });
